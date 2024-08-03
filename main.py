19f65b5f63998f0ab038a59de7c0bfab876d59b9
Joe Moncrieff <joseph.moncrieff@gmail.com>
2024-08-03 20:46:21 +0100

    Major project cleanup

--- main.py
+++ main.py
@@ -5,7 +5,27 @@ import sounddevice as sd
 import numpy as np
 from PIL import Image, ImageTk
 
-curr_colour = 'red'
+windows_thresholds = [0.03,0.12,0.35]
+mac_thresholds = [0.002,0.02,0.04 ]
+
+#change this depending on device
+current_thresholds = mac_thresholds.copy()
+
+
+
+""" TODO:
+* Merge Mac and Windows Files / Check for any breakpoints
+* investigate "Input overflow" Errors
+    --> Seems to be an issue mainly with the blinkng part (I think)
+    --> Maybe a Mac only problem? 
+* set up a file to establish microphone breakpoints.
+* Add '(Helpful)' comments
+
+* Add art customisation (Maybe Hats? maybe on obs? check feasibility)
+
+"""
+
+
 canvas_dimension = 350
 
 sx, sy = int(canvas_dimension/2), int(canvas_dimension/2)
@@ -19,8 +39,7 @@ root = Tk()
 # Make the root window always on top
 root.wm_attributes("-topmost", True)
 
-
-root.wm_attributes("-transparent", "cyan")
+# root.wm_attributes("-transparent", "cyan")
 # Set the root window background color to a transparent color
 root.config(bd=0, bg="cyan")
 
@@ -39,15 +58,6 @@ mo = ImageTk.PhotoImage(mo.resize((canvas_dimension, canvas_dimension)))
 mw = ImageTk.PhotoImage(mw.resize((canvas_dimension, canvas_dimension)))
 ms = ImageTk.PhotoImage(ms.resize((canvas_dimension, canvas_dimension)))
 
-
-'''
-mc = PhotoImage(file="Photos/Layers/MouthiClosed.png")
-mos = PhotoImage(file="Photos/Layers/MouthiOpenSlight.png")
-mo = PhotoImage(file="Photos/Layers/MouthiOpen.png")
-mw = PhotoImage(file="Photos/Layers/MouthiOpenWider.png")
-ms = PhotoImage(file="Photos/Layers/MouthiSmile.png")
-'''
-
 mouth_arr.append(mc)
 mouth_arr.append(mos)
 mouth_arr.append(mo)
@@ -61,17 +71,10 @@ eh = Image.open("Photos/Layers/EyesiHalfOpen.png")
 eo = Image.open("Photos/Layers/EyesiOpen.png")
 es = Image.open("Photos/Layers/EyesiLaughter.png")
 
-ec = ImageTk.PhotoImage(ec.resize((canvas_dimension, canvas_dimension), Image.ANTIALIAS))
-eh = ImageTk.PhotoImage(eh.resize((canvas_dimension, canvas_dimension), Image.ANTIALIAS))
-eo = ImageTk.PhotoImage(eo.resize((canvas_dimension, canvas_dimension), Image.ANTIALIAS))
-es = ImageTk.PhotoImage(es.resize((canvas_dimension, canvas_dimension), Image.ANTIALIAS))
-
-'''
-ec = PhotoImage(file="Photos/Layers/EyesiClosed.png")
-eh = PhotoImage(file="Photos/Layers/EyesiHalfOpen.png")
-eo = PhotoImage(file="Photos/Layers/EyesiOpen.png")
-es = PhotoImage(file="Photos/Layers/EyesiLaughter.png")
-'''
+ec = ImageTk.PhotoImage(ec.resize((canvas_dimension, canvas_dimension), Image.Resampling.LANCZOS))
+eh = ImageTk.PhotoImage(eh.resize((canvas_dimension, canvas_dimension), Image.Resampling.LANCZOS))
+eo = ImageTk.PhotoImage(eo.resize((canvas_dimension, canvas_dimension), Image.Resampling.LANCZOS))
+es = ImageTk.PhotoImage(es.resize((canvas_dimension, canvas_dimension), Image.Resampling.LANCZOS))
 
 eye_arr.append(eo)
 eye_arr.append(eh)
@@ -79,12 +82,6 @@ eye_arr.append(ec)
 
 
 fig_arr = []
-'''
-fc = PhotoImage(file="Photos/Layers/FigureiColour.png")
-fo = PhotoImage(file="Photos/Layers/FigureiOutline.png")
-bd = PhotoImage(file="Photos/Layers/BoxiDetails.png")
-bs = PhotoImage(file="Photos/layers/BoxiSign.png")
-'''
 
 fc = Image.open("Photos/Layers/FigureiColour.png")
 fo = Image.open("Photos/Layers/FigureiOutline.png")
@@ -100,15 +97,20 @@ bs = ImageTk.PhotoImage(bs.resize((canvas_dimension, canvas_dimension)))
 fig_arr.append(fc)
 fig_arr.append(fo)
 fig_arr.append(bd)
+
+#TODO: Move Box sign to (Future) Accessories section. 
 #fig_arr.append(bs)
 
 
 # Setting up main body image here
 my_canvas = Canvas(root, width=canvas_dimension, height=canvas_dimension, highlightthickness=0, bg=root['bg'], bd=0)
 my_canvas.pack()
+
+#Initial build of character model
 for img in fig_arr:
     my_canvas.create_image(sx, sy, image=img, tags="figure")
-#Adding open eyes here for now
+
+#Adding open eyes here
 my_canvas.create_image(sx, sy, image=eye_arr[0], tags="eyes")
 my_canvas.create_image(sx, sy, image=mouth_arr[1], tags="mouth")
 
@@ -122,7 +124,6 @@ eye_state = 0
 def audio_callback(indata, frames, time, status):
 
     global elapsed
-    global label # For testing, To remove.
     global current_state
 
     #Eye Trackers
@@ -130,9 +131,11 @@ def audio_callback(indata, frames, time, status):
     global eye_state
 
 
-    # Current state is here because we have a
-    # conundrum where we want to go up intensity
-    # immediately but down in intensity kind of slowly
+    """ 
+    Current state is here because we have a
+    conundrum where we want to go up intensity (of expressions) (Quiet --> Loud)
+    immediately but down in intensity kind of slowly (Loud ---> Quiet) 
+    """
 
     if elapsed == 0.0:
         elapsed = t.time()
@@ -140,15 +143,20 @@ def audio_callback(indata, frames, time, status):
     if eyes_elapsed == 0.0:
         eyes_elapsed = t.time()
     """This is called (from a separate thread) for each audio block."""
+    
+    #TODO: Input overflow printed from here
     if status:
         print(status, file=sys.stderr)
 
     # Fancy indexing with mapping creates a (necessary!) copy:
     avrg = float(np.sum(np.abs(indata)))/float(np.size(indata))
-    print(avrg)
+    
+    # "avrg" printing should help with threshold values
+    # print(avrg)
     # print(t.time())
 
-    if 0.03 < avrg < 0.12 and (t.time() - elapsed >= 0.5 or current_state <= 1):
+    if current_thresholds[0] < avrg < current_thresholds[1] and (t.time() - elapsed >= 0.5 or current_state <= 1):
+        
         if current_state == 3:
             my_canvas.delete('eyes')
             my_canvas.create_image(sx, sy, image=eye_arr[0], tags="eyes")
@@ -159,7 +167,7 @@ def audio_callback(indata, frames, time, status):
         elapsed = t.time()
         current_state = 1
 
-    elif 0.35 > avrg > 0.12 and (t.time() - elapsed >= 0.4 or current_state <= 2):
+    elif current_thresholds[2] > avrg > current_thresholds[1] and (t.time() - elapsed >= 0.4 or current_state <= 2):
         if current_state == 3:
             my_canvas.delete('eyes')
             my_canvas.create_image(sx, sy, image=eye_arr[0], tags="eyes")
@@ -170,7 +178,7 @@ def audio_callback(indata, frames, time, status):
         elapsed = t.time()
         current_state = 2
 
-    elif avrg >= 0.35:
+    elif avrg >= current_thresholds[2]:
         if current_state != 3:
             my_canvas.delete('mouth')
             my_canvas.create_image(sx, sy, image=mouth_arr[4], tags="mouth")
@@ -202,13 +210,14 @@ def audio_callback(indata, frames, time, status):
 # End Audio Callback =============================
 
 
+#helper function for the blink.
 def eye_blink(eye_state_, time_diff, canvas):
     global eyes_elapsed
 
     if eye_state_ > 0:
         if time_diff > 0.12:
             new_eye_state = (eye_state_ + 1) % len(eye_arr)
-            print(time_diff)
+            # print(time_diff)
             canvas.delete('eyes')
             canvas.create_image(sx, sy, image=eye_arr[new_eye_state], tags="eyes")
             eyes_elapsed = t.time()
@@ -223,11 +232,29 @@ def eye_blink(eye_state_, time_diff, canvas):
 
 
 
+#General device notes:
+
+"""
+You can run `python3 -m sounddevice to find what channel the input device you actually
+want to use is on.
+"""
+
+""" Windows Settings
 stream = sd.InputStream(
         device=1,
         channels=1,
         samplerate=44100.0,
         callback=audio_callback)
+"""
+
+""" Mac Settings """
+stream = sd.InputStream(
+        device=0,
+        channels=1,
+        samplerate=44100.0,
+        callback=audio_callback)
+
+
 
 with stream:
     root.mainloop()
